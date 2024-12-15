5affc706e4acbf8efecb936de538474708e9874b
raghad <raghad.bjaad@gmail.com>
2024-12-15 14:25:48 +0300

    Initial Commit

---
+++ MyStory/MyStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStoryApp.swift
+//  MyStory
+//
+//  Created by Raghad on 14/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct MyStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyStoryTests/MyStoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyStoryTests.swift
+//  MyStoryTests
+//
+//  Created by Raghad on 14/06/1446 AH.
+//
+
+import Testing
+@testable import MyStory
+
+struct MyStoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
